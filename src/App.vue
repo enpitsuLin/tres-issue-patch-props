430612fac903f6d4aed5377e487e84879f1d027b
enpitsulin <enpitsulin@gmail.com>
2023-08-17 16:20:21 +0800

    repo

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { TresCanvas } from '@tresjs/core'
+import Box from './Box.vue'
+import { OrbitControls } from '@tresjs/cientos';
+</script>
+
+<template>
+  <TresCanvas window-size>
+    <OrbitControls />
+    <Box></Box>
+  </TresCanvas>
+</template>
+ 
---
+++ src/Box.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <TresMesh>
+    <TresMeshBasicMaterial />
+    <!-- change args props see mesh don't update -->
+    <TresBoxGeometry :args="[2, 2, 2]" />
+  </TresMesh>
+</template>
